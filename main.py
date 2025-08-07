f8a8eba2c2b063f6e8be14ed393007aac70ed7f5
Sai2475 <saisanjay3000@gmail.com>
2025-08-07 22:45:28 +0530

    added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from langchain.chains import LLMChain
 from langchain_groq import ChatGroq
 import os
 
-llm = ChatGroq(
+llm_resto = ChatGroq(
     api_key="",
     model_name="llama3-70b-8192-verstaile"
     temperature = 0.0
@@ -12,6 +12,22 @@ llm = ChatGroq(
 prompt_template_resto = PromptTemplate(
     input_variables=['age','gender','weight','height','veg_or_nonveg','disease','region','foodtype'],
     template=(
-        
-    )
+        "Diet Recommendation System:\n"
+        "I want you to provide output in the following format using the input criteria:\n\n"
+        "Restaurants:\n"
+        "- name1\n- name2\n- name3\n- name4\n- name5\n- name6\n\n"
+        "Breakfast:\n"
+        "- item1\n- item2\n- item3\n- item4\n- item5\n- item6\n\n"
+        "Dinner:\n"
+        "- item1\n- item2\n- item3\n- item4\n- item5\n\n"
+        "Workouts:\n"
+        "- workout1\n- workout2\n- workout3\n- workout4\n- workout5\n- workout6\n\n"
+        "Criteria:\n"
+        "Age: {age}, Gender: {gender}, Weight: {weight} kg, Height: {height} ft, "
+        "Vegetarian: {veg_or_nonveg}, Disease: {disease}, Region: {region}, "
+        "Allergics: {allergics}, Food Preference: {foodtype}.\n"
+
+ )
 )
+
+chain = LLMChain(llm = llm_resto, prompt = prompt_template_resto)
