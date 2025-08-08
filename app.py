fe83e5d09368fef034be405637b2a09241f9811b
Sai2475 <saisanjay3000@gmail.com>
2025-08-08 22:50:33 +0530

    added

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from langchain.prompts import PromptTemplate 
+from langchain.chains import LLMChain
+from langchain_groq import ChatGroq
+import os
+import re
+
+
+app = Flask(__name__)
+
+app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
