d4b2f43e3960b4b6c328d7abf696890fef86f7a2
Sai2475 <saisanjay3000@gmail.com>
2025-08-08 23:08:24 +0530

    added

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ from langchain_groq import ChatGroq
 import os
 import re
 
-
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
